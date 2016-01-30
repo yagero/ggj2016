4fa3bb25e6d61b6b972f68d47d1c08a4a478be12
Barbara <barbara@hunnius.net>
2016-01-30 20:08:12 +0100

    Menu Screen

--- ggj16/Assets/Scripts/LevelManager.cs
+++ ggj16/Assets/Scripts/LevelManager.cs
@@ -3,7 +3,35 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+
+	private GameObject panel;
+
+	void Start(){
+		panel =  GameObject.Find("Credits");
+	
+	}
+
 	public void loadScene(string name){
 		Application.LoadLevel (name);
+	}
+
+	public void openPanel(string name){
+		Debug.Log (name);
+		panel =  GameObject.Find(name);
+
+		panel.GetComponent<CanvasGroup>().alpha = 1;
+		panel.GetComponent<CanvasGroup>().interactable = true;
+		panel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+		//GetComponent< Renderer >().enabled = false;
+	}
+
+	public void closePanel(string name){
+		panel =  GameObject.Find(name);
+
+		panel.GetComponent<CanvasGroup>().alpha = 0;
+		panel.GetComponent<CanvasGroup>().interactable = false;
+		panel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
 	}
 }
