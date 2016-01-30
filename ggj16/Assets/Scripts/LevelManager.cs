f8b60c94c61932ac65af8d513ec35457b734d6c3
Barbara <barbara@hunnius.net>
2016-01-30 14:25:56 +0100

    Set up forest and controlls

---
+++ ggj16/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void loadScene(string name){
+		Application.LoadLevel (name);
+	}
+}
---
+++ ggj16/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			var tZ = Time.deltaTime * speed;
+			transform.Translate(0,0,tZ);
+		}
+		if (Input.GetKey ("down")) {
+			var tZ = Time.deltaTime * speed;
+			transform.Translate(0,0,-tZ);
+		}
+		if (Input.GetKey ("left")) {
+			var tZ = Time.deltaTime * speed * 2;
+			transform.Rotate(0,-tZ,0);
+		}
+		if (Input.GetKey ("right")) {
+			var tZ = Time.deltaTime * speed * 2;
+			transform.Rotate(0,tZ,0);
+		}
+	}
+}
