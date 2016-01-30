c91aee161795c8e1c1cd6eb41e94bb7564a705d8
Barbara <barbara@hunnius.net>
2016-01-30 18:14:25 +0100

    Shading, Terrain etc

---
+++ ggj16/Assets/Scripts/CameraShader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.SetReplacementShader (Shader.Find ("Toon/Basic Outline"), "RenderType");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- ggj16/Assets/Scripts/PlayerScript.cs
+++ ggj16/Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 
 	public int speed = 10;
+	public float speedH = 2.0f;
+	public float speedV = 2.0f;
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -28,5 +33,10 @@ public class PlayerScript : MonoBehaviour {
 			var tZ = Time.deltaTime * speed * 2;
 			transform.Rotate(0,tZ,0);
 		}
+
+		yaw += speedH * Input.GetAxis("Mouse X");
+		pitch -= speedV * Input.GetAxis("Mouse Y");
+
+		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 	}
 }
