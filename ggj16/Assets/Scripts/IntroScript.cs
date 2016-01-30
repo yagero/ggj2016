c2a1d6195ef588c1dfee8b03959114ac4130d68f
Barbara <barbara@hunnius.net>
2016-01-30 22:01:07 +0100

    Intro & Sounds

---
+++ ggj16/Assets/Scripts/IntroScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class IntroScript : MonoBehaviour {
+
+		public AudioClip firstSentence;
+		public AudioClip secondSentence;
+		public AudioClip thirdSentence;
+		void Start()
+		{
+			GetComponent<AudioSource> ().loop = false;
+			StartCoroutine(playEngineSound());
+		}
+
+		IEnumerator playEngineSound()
+		{
+			GetComponent<AudioSource> ().clip = firstSentence;
+			GetComponent<AudioSource> ().Play();
+			yield return new WaitForSeconds(GetComponent<AudioSource> ().clip.length);
+			GetComponent<AudioSource> ().clip = secondSentence;
+			GetComponent<AudioSource> ().Play();
+			yield return new WaitForSeconds(GetComponent<AudioSource> ().clip.length);
+			GetComponent<AudioSource> ().clip = thirdSentence;
+			GetComponent<AudioSource> ().Play();
+			yield return new WaitForSeconds(GetComponent<AudioSource> ().clip.length);
+			Application.LoadLevel ("Game");
+		}
+}
