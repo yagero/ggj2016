100ac941342948bf25c89c68e2f52056f6a35a7f
Barbara <barbara@hunnius.net>
2016-01-31 10:01:31 +0100

    Fonts & Color

--- ggj16/Assets/Scripts/IngredientColor.cs
+++ ggj16/Assets/Scripts/IngredientColor.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class IngredientColor : MonoBehaviour {
-	public Color color = Color.black;
+	Color color = Color.black;
 	GameObject player;
 	float distance;
+	string itemName;
 
+	bool colorChanging;
 
 	// Use this for initialization
 	void Start () {
@@ -13,24 +15,56 @@ public class IngredientColor : MonoBehaviour {
 		color.r = 185f;
 		color.b = 185f;
 		//calculateDistance ();
+		colorChanging = false;
+
+
+		Debug.Log(name);
 
 		player = GameObject.FindGameObjectWithTag("MainCamera");
 	}
-	
+
+	private float lastdistance = 0;
+
 	// Update is called once per frame
 	void Update () {
 		distance = Vector3.Distance(player.transform.position, transform.position);
 
-	}
+		if (distance < lastdistance) { 
+			if (colorChanging == false) {	
 
+				StartCoroutine(changeColorBright ());
+				colorChanging = true;
+			}
+		} else if (distance > lastdistance) {
+			if (colorChanging == false) {	
 
-	void changeColor(){
-
-		while (distance < 100) {
-			color.g = color.g - (float)2f;
-			color.b = color.b - (float)2f;
+				StartCoroutine(changeColorDark ());
+				colorChanging = true;
+			}
 		}
+		lastdistance = distance; // don't forget to update last distance! 
 
-		GetComponent<Renderer>().material.color = new Color (color.r,color.g,color.b);
+
 	}
+
+
+	IEnumerator changeColorBright(){
+		color.g = color.g - (float)5f;
+		color.b = color.b - (float)5f;
+		Debug.Log (color.g);
+		GetComponent<Renderer> ().material.color = new Color (color.r/255, color.g/255, color.b/255);
+		yield return new WaitForSeconds (0.5f);
+		colorChanging = false;
+	}
+
+	IEnumerator changeColorDark(){
+		color.g = color.g + (float)5f;
+		color.b = color.b + (float)5f;
+		GetComponent<Renderer> ().material.color = new Color (color.r/255, color.g/255, color.b/255);
+		yield return new WaitForSeconds (0.5f);
+		colorChanging = false;
+	}
+
+
+
 }
